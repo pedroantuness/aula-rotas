8dd846c2e6247b6482049d097fb6aa949eb4db6f
pedroantuness <RM551409@fiap.com.br>
2023-09-05 16:06:12 -0300

    criado o component menu para menu de navegação

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import { useState } from 'react'
 import { Outlet } from 'react-router-dom'
 import './App.css'
 
+import Menu from './components/Menu'
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
 
     <>
-
+      <Menu />
       <Outlet />
       
     </>
